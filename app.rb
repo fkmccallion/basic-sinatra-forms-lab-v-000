b8d6f3e6cf616d16dd9bd440ae1babef4330876b
Frank McCallion <fkmccallion@gmail.com>
2018-04-10 20:13:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,15 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post 'team' do
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @pf = params["pf"]
+    @sf = params["sf"]
+    @c = params["c"]
+    
     erb :team
   end
 
